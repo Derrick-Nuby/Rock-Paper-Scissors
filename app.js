f23b09ca98eeee65ff2c6cbc97ed8b682b26bc27
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-07-16 21:08:31 +0200

    changing the conditions and determining the datatypes of returns

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const arrayOfChoices = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
-  const arrayOfChoices = ["rock", "paper", "scissors"];
-  console.log(arrayOfChoices[choice]);
-  return arrayOfChoices[choice];
+  //   console.log(arrayOfChoices[choice]);
+  return choice;
 }
 
 function getUserChoice() {
@@ -11,23 +12,29 @@ function getUserChoice() {
   );
 
   if (userChoice.toLowerCase() === "rock") {
-    console.log("rock");
-    return "rock";
+    // console.log("rock");
+    return 0;
   } else if (userChoice.toLowerCase() === "paper") {
-    console.log("paper");
-    return "paper";
+    // console.log("paper");
+    return 1;
   } else if (userChoice.toLowerCase() === "scissors") {
-    console.log("scissors");
-    return "scissors";
+    // console.log("scissors");
+    return 2;
   } else {
     console.log("The biggest error you have ever made");
   }
 }
-playerSelection = getUserChoice();
+
 computerSelection = getComputerChoice();
+playerSelection = getUserChoice();
 
-console.log("the player selection is: ", playerSelection);
-console.log("the computer selection is: ", computerSelection);
+console.log(typeof arrayOfChoices[computerSelection]);
+console.log(typeof arrayOfChoices[playerSelection]);
 
-function playRound(playerSelection, computerSelection) {}
-playRound();
+console.log("the computer selection is: ", arrayOfChoices[computerSelection]);
+console.log("the player selection is: ", arrayOfChoices[playerSelection]);
+
+// function playRound(playerSelection, computerSelection) {
+//     if
+// }
+// playRound();
