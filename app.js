8016ac35026bd9116b4a73213debbcb3549ab6fb
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-07-16 20:24:11 +0200

    Added the functionality for getting user input and computer input

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  const arrayOfChoices = ["rock", "paper", "scissors"];
+  console.log(arrayOfChoices[choice]);
+  return arrayOfChoices[choice];
+}
+
+function getUserChoice() {
+  let userChoice = prompt(
+    "Enter your choice between: rock, paper and scissors"
+  );
+
+  if (userChoice.toLowerCase() === "rock") {
+    console.log("rock");
+    return rock;
+  } else if (userChoice.toLowerCase() === "paper") {
+    console.log("paper");
+    return paper;
+  } else if (userChoice.toLowerCase() === "scissors") {
+    console.log("scissors");
+    return scissors;
+  } else {
+    console.log("The biggest error you have ever made");
+  }
+}
+
+getComputerChoice();
+getUserChoice();
